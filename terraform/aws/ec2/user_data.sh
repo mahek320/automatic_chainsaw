f3c23a3474469c7a07b7df32f154b20f7c86a186
mahek320 <mahekshetty2001@gmail.com>
2025-12-25 16:35:42 +0530

    Add wget installation for Jenkins setup

--- terraform/aws/ec2/user_data.sh
+++ terraform/aws/ec2/user_data.sh
@@ -57,6 +57,7 @@ yum install -y git
 yum install -y java-17-amazon-corretto-headless
 
 # Add Jenkins repository and install Jenkins
+yum install -y wget
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
